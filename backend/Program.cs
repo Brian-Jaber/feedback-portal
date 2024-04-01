d2e16bbc27a57ede05575923262d8a4c5a741cdc
Brian Jaber <brianjaber@gmail.com>
2024-03-31 20:56:00 -0700

    Updates Program.cs file.
    1. Imports EF Core package, DBContext, and DotNetEnv package.
    2. Applies and configures DbContext for MySqlServer.

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using FeedBackPortal.Data;
+using DotNetEnv;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+Env.Load();
+
+builder.Services.AddDbContext<AppDbContext>(options => 
+    options.UseMySql(builder.Configuration.GetConnectionString("MySqlConnection"),
+    new MySqlServerVersion(new Version(8, 0, 1))));
+
+
+builder.Services.AddControllers();  // For Controllers
+
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
